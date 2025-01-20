3f29d8eb02a3abf80fa90a8757beb937eeb98771
Sujan Ghimire <ghimire.amrit147@gmail.com>
2025-01-20 20:09:14 +0545

    scoll when slider is selected

--- hero-slider.js
+++ hero-slider.js
@@ -16,4 +16,20 @@ document.addEventListener("DOMContentLoaded", () => {
     effect: "slide",
     speed: 1000,
   });
+
+  const swiperSection = document.getElementById("swiper-section");
+  const topOffset = 200;
+  document.querySelectorAll(".slide-image").forEach((image) => {
+    image.addEventListener("click", (event) => {
+      const targetIndex = parseInt(event.target.getAttribute("data-index"), 10);
+      const sectionPosition =
+        swiperSection.getBoundingClientRect().top + window.scrollY - topOffset;
+      window.scrollTo({
+        top: sectionPosition,
+        behavior: "smooth",
+      });
+
+      swiper.slideTo(targetIndex);
+    });
+  });
 });
